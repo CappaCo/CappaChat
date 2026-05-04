cf851745d232aeaed6bca8a65976836749d565be
Casper Howse <casperhowse@gmail.com>
2026-05-05 11:41:27 +1200

    feat: add about page

---
+++ routes/(pages)/about.tsx
@@ -0,0 +1,15 @@
+import { Head } from "fresh/runtime";
+
+import { define } from "@/utils.ts";
+
+export default define.page(function Home() {
+
+    return (
+        <>
+            <Head>
+                <title>About page</title>
+            </Head>
+            <h1>About page</h1>
+        </>
+    );
+});
