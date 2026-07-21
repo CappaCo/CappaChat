f99b5102428e650415df6acc4d5e1c2ea30fcda9
Casper Howse <casperhowse@gmail.com>
2026-07-21 18:50:33 +1200

    feat: update chat page styling

--- islands/MessagesDisplay.tsx
+++ islands/MessagesDisplay.tsx
@@ -1,12 +1,65 @@
 import { Id, Message, User } from "@/lib/types.ts";
 
-function MessageElement({ message, user }: { message: Message; user?: User }) {
+export default function MessagesDisplay(
+    { messages, users }: { messages?: Message[]; users?: User[] },
+) {
+    const usersMap = new Map<Id, User>();
+
+    if (users !== undefined) {
+        for (const user of users) {
+            usersMap.set(user.id, user);
+        }
+    } else {
+        console.error("no users???");
+    }
+
+    return (
+        <section id="messages-container">
+            <ol id="messages">
+                {(() => {
+                    if (messages === undefined) {
+                        return <MessagesLoadingSkeleton />;
+                    }
+                    if (messages.length === 0) return "No messages";
+
+                    return messages.map((message, index) => {
+                        const user = usersMap.get(message.authorId);
+                        const prevMessage = (index !== messages.length)
+                            ? messages[index - 1]
+                            : undefined;
+
+                        return (
+                            <MessageElement
+                                key={message.id}
+                                message={message}
+                                prevMessage={prevMessage}
+                                user={user}
+                            />
+                        );
+                    });
+                })()}
+            </ol>
+        </section>
+    );
+}
+
+function MessagesLoadingSkeleton() {
+    return "Messages loading skeleton...";
+}
+
+function MessageElement(
+    { message, user, prevMessage }: {
+        message: Message;
+        user?: User;
+        prevMessage?: Message;
+    },
+) {
     // TODO: check this with attachments later on
     if (!message.content) {
         return (
-            <p class="message">
+            <li class="message">
                 <em>No message content</em>
-            </p>
+            </li>
         );
     }
 
@@ -18,6 +71,7 @@ function MessageElement({ message, user }: { message: Message; user?: User }) {
     const now = new Date();
     const dateSent = new Date(message.createdAt);
     const verbosityThresholdDate = now;
+    // TODO: make this into a helper function that checks if something is more than a day before
     verbosityThresholdDate.setDate(verbosityThresholdDate.getDate() - 1);
 
     let timestamp = dateSent.toLocaleTimeString();
@@ -26,60 +80,38 @@ function MessageElement({ message, user }: { message: Message; user?: User }) {
         timestamp = dateSent.toLocaleDateString() + " " + timestamp;
     }
 
-    return (
-        <div class="message text">
-            {/* TODO: add user profile picture into this */}
-            <img src={pfpURL} />
-            <div class="message-but-not-image">
-                <span class="message-header">
-                    <span class="message-username">{username}</span>
-                    <small class="message-timestamp">{timestamp}</small>
-                </span>
-                <p class="message-content">{message.content}</p>
-            </div>
-        </div>
-    );
-}
-
-function MessagesLoadingSkeleton() {
-    return "Messages loading skeleton...";
-}
-
-export default function MessagesDisplay(
-    { messages, users }: { messages?: Message[]; users?: User[] },
-) {
-    const usersMap = new Map<Id, User>();
-
-    if (users !== undefined) {
-        for (const user of users) {
-            usersMap.set(user.id.trim(), user);
-        }
-        console.log(usersMap);
+    function shouldGroupMessage() {
+        if (prevMessage === undefined) return false;
+        if (message.authorId !== prevMessage.authorId) return false;
+        const lastMessageButABitAfter = new Date(prevMessage.createdAt);
+        lastMessageButABitAfter.setHours(verbosityThresholdDate.getHours() - 1);
+        if (dateSent < lastMessageButABitAfter) return false;
+        return true;
     }
 
+    const groupMessage = shouldGroupMessage();
+
+    const messageClasses = [
+        "message",
+        "text",
+    ];
+
+    if (groupMessage) {
+        messageClasses.push("group");
+    }
+    
     return (
-        <div id="messages-container">
-            <section id="messages">
-                {(() => {
-                    if (messages === undefined) {
-                        return <MessagesLoadingSkeleton />;
-                    }
-                    if (messages.length === 0) return "No messages";
-
-                    return messages.map((message) => {
-                        const user = usersMap.get(message.authorId);
-                        console.log("got user:", user);
-
-                        return (
-                            <MessageElement
-                                key={message.id}
-                                message={message}
-                                user={user}
-                            />
-                        );
-                    });
-                })()}
-            </section>
-        </div>
+        <li class={messageClasses.join(" ")}>
+            {groupMessage ? null : <img src={pfpURL} />}
+            <div class="message-body">
+                {groupMessage ? null : (
+                    <h3 class="message-header">
+                        <span class="message-username">{username}</span>
+                        <small class="message-timestamp">{timestamp}</small>
+                    </h3>
+                )}
+                <p class="message-content">{message.content}</p>
+            </div>
+        </li>
     );
 }
