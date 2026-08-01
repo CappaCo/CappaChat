5052516aacd3f78d95770fb793370c3b5861dca8
Casper Howse <casperhowse@gmail.com>
2026-08-01 16:53:53 +1200

    chore: remove old devWebsocketServer.ts

--- devWebsocketServer.ts
+++
@@ -1,5 +0,0 @@
-import { handleWebsocketConnection } from "@/lib/websocket.ts";
-
-// TODO: remove this
-const wsServer = Deno.serve(handleWebsocketConnection);
-console.log("wsServer started:", wsServer.addr);
