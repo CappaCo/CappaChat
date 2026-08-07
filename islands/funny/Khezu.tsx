7679aa319dd57f2039ffd0e36916774b888dbfb8
Casper Howse <22231@northcote.school.nz>
2026-08-07 15:11:37 +1200

    feat: added khezu mode

--- islands/funny/Khezu.tsx
+++ islands/funny/Khezu.tsx
@@ -3,21 +3,45 @@ import { settings } from "@/stores/settings.ts";
 
 export function KhezuAppearing() {
     const khezuRef = useRef<HTMLImageElement>(null);
+    let sound: HTMLAudioElement | undefined;
 
-    function showKhezu() {
-        const khezu = khezuRef.current;
-        if (khezu === null) return;
-        const chance = 1 / 10;
-        Math.random() * 100;
-    }
-
-    function killKhezu() {
-        const khezu = khezuRef.current;
-        if (khezu === null) return;
-    }
+    const chance = 1 / 10_000; // 1 in 10,000 chance
+    const perTime = 1 * 1_000; // per second of khezu appearing
+    const showTime = 1 * 1_000; // for 1 second
 
     function randomKhezu() {
+        const number = Math.random();
+
+        if (number <= chance) {
+            if (khezuRef.current === null) return;
+            khezuRef.current.style.display = "block";
+
+            if (sound) {
+                sound.play();
+            }
+
+            setTimeout(() => {
+                if (khezuRef.current === null) return;
+                khezuRef.current.style.display = "none";
+            }, showTime);
+        }
+
+        if (settings.value.funny.khezu) setTimeout(randomKhezu, perTime);
     }
 
-    return <img src="/static/funny/khezu.webp" />;
+    useEffect(randomKhezu, [settings.value.funny.khezu]);
+
+    useEffect(() => {
+        sound = new Audio("/funny/beefyscream.ogg");
+        console.log("khezu roar loaded");
+    }, []);
+
+    return (
+        <img
+            ref={khezuRef}
+            id="khezu"
+            src="/funny/khezu.webp"
+            style="display: none;"
+        />
+    );
 }
--- routes/_app.tsx
+++ routes/_app.tsx
@@ -2,6 +2,7 @@ import { asset } from "fresh/runtime";
 
 import { define } from "@/lib/utils.ts";
 import BugWalkingAcrossScreen from "@/islands/funny/BugWalkingAcrossScreen.tsx";
+import { KhezuAppearing } from "@/islands/funny/Khezu.tsx";
 
 export default define.page(function ({ Component }) {
     return (
@@ -34,6 +35,7 @@ export default define.page(function ({ Component }) {
             </head>
             <body>
                 <BugWalkingAcrossScreen />
+                <KhezuAppearing />
                 <Component />
             </body>
         </html>
