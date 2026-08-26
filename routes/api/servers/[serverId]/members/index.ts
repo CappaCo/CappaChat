efc404b9f35c17a892eb59d2c7c2305c1eae35c6
Casper Howse <22231@northcote.school.nz>
2026-08-27 09:59:36 +1200

    feat: add member joining endpoint

--- routes/api/servers/[serverId]/members/index.ts
+++ routes/api/servers/[serverId]/members/index.ts
@@ -1,6 +1,8 @@
 import { define } from "@/lib/utils.ts";
 import { getUsersInServer } from "@/lib/db/server.ts";
 import { hasPermission } from "@/lib/db/permissions.ts";
+import { joinUser } from "@/lib/db/members.ts";
+import { getUserIdFromUsername } from "@/lib/db/user.ts";
 
 export const handler = define.handlers({
     async GET(ctx) {
@@ -27,7 +29,7 @@ export const handler = define.handlers({
 
         return new Response(JSON.stringify(members));
     },
-    POST(ctx) {
+    async POST(ctx) {
         const serverId = ctx.state.serverId;
 
         const user = ctx.state.requestingUser;
@@ -47,10 +49,50 @@ export const handler = define.handlers({
             );
         }
 
-        // TODO: implement this
-        console.log("joining user to server:", serverId);
-        return new Response(JSON.stringify({
-            message: "ok",
-        }));
+        const formData = await ctx.req.formData();
+        console.log({formData});
+
+        const username = formData.get("username");
+
+        if (username === null) {
+            return new Response(
+                JSON.stringify({ message: "no user name in form data" }),
+                { status: 400 },
+            );
+        }
+
+        const name = username.toString().trim();
+
+        if (name === "") {
+            return new Response(
+                JSON.stringify({ message: "user name is empty" }),
+                { status: 400 },
+            );
+        }
+
+        const userId = await getUserIdFromUsername(name);
+
+        if (userId === undefined) {
+            return new Response(
+                JSON.stringify({ message: "user not found" }),
+                { status: 404 },
+            );
+        }
+
+        // TODO: add logic to check if user is already in there
+        /*
+        if (e.name === "PostgresError") {
+            // SOMETHING
+        }
+        throw e;
+        */
+        await joinUser(userId, serverId);
+
+        return new Response(
+            JSON.stringify({
+                message: "ok",
+            }),
+            { status: 201 },
+        );
     },
 });
